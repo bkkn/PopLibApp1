ce58db799f37adac9373445783c26f71fe76e515
bkkn <bakaikin@yandex.ru>
2022-05-01 23:49:25 +0300

    dependencies set up; viewBinding added, by ViewModels() added

--- app/src/main/java/me/bkkn/poplibapp1/MainActivity.kt
+++ app/src/main/java/me/bkkn/poplibapp1/MainActivity.kt
@@ -4,15 +4,4 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import me.bkkn.poplibapp1.ui.main.MainFragment
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.main_activity)
