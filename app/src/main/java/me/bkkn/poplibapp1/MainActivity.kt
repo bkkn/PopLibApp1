3dbc9135360b669dd9c7ba5ea21df69127093ffb
bkkn <bakaikin@yandex.ru>
2022-05-19 16:34:27 +0300

    Themes change implemented

--- app/src/main/java/me/bkkn/poplibapp1/MainActivity.kt
+++ app/src/main/java/me/bkkn/poplibapp1/MainActivity.kt
@@ -1,5 +1,27 @@
 package me.bkkn.poplibapp1
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(R.layout.main_activity)
+class MainActivity : AppCompatActivity() {
+
+    private val sharedPreferencesKey = "shared preferences key"
+    private val themeKey = "theme key"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(getCurrentTheme())
+        setContentView(R.layout.main_activity)
+    }
+
+    fun getCurrentTheme(): Int {
+        val sharedPreferences = getSharedPreferences(sharedPreferencesKey, MODE_PRIVATE)
+        return sharedPreferences.getInt(themeKey, -1)
+    }
+
+    fun setCurrentTheme(currentTheme: Int) {
+        val sharedPreferences = getSharedPreferences(sharedPreferencesKey, MODE_PRIVATE)
+        sharedPreferences.edit().putInt(themeKey, currentTheme).apply()
+        recreate()
+    }
+}
--- app/src/main/java/me/bkkn/poplibapp1/ui/MainViewModel.kt
+++ app/src/main/java/me/bkkn/poplibapp1/ui/MainViewModel.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import me.bkkn.myfirstmaterialapp.api.PictureOfTheDayResponse
 import me.bkkn.myfirstmaterialapp.domain.NasaRepository
 import me.bkkn.poplibapp1.Const.network_error
 import me.bkkn.poplibapp1.Util
@@ -19,57 +20,21 @@ class MainViewModel(private val repository: NasaRepository) : ViewModel() {
     private val _loading = MutableStateFlow(false)
     val loading: Flow<Boolean> = _loading
 
-    private val _image: MutableStateFlow<String?> = MutableStateFlow(null)
-    val image: Flow<String?> = _image
-
-    private val _title: MutableStateFlow<String?> = MutableStateFlow(null)
-    val title: Flow<String?> = _title
-
-    private val _explanation: MutableStateFlow<String?> = MutableStateFlow(null)
-    val explanation: Flow<String?> = _explanation
+    private val _response: MutableSharedFlow<PictureOfTheDayResponse?> = MutableSharedFlow()
+    val response: Flow<PictureOfTheDayResponse?> = _response
 
     private val _error: MutableSharedFlow<String> = MutableSharedFlow()
     val error: Flow<String> = _error
 
     @RequiresApi(Build.VERSION_CODES.O)
-    fun requestPictureOfTheDay() {
-
-        _loading.value = true
-
-        viewModelScope.launch {
-            try {
-                val url = repository.pictureOfTheDay().url
-                _image.emit(url)
-                val title = repository.pictureOfTheDay().title
-                _title.emit(title)
-                val expl = repository.pictureOfTheDay().explanation
-                _explanation.emit(expl)
-            } catch (e: IOException) {
-                _error.emit(network_error)
-            }
-
-            _loading.emit(false)
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
     fun requestPictureByDate(delta: Long) {
-
         _loading.value = true
-
         viewModelScope.launch {
             try {
-                val date = Util.getDateString(delta)
-                val url = repository.pictureByDate(date).url
-                _image.emit(url)
-                val title = repository.pictureByDate(date).title
-                _title.emit(title)
-                val explanation = repository.pictureByDate(date).explanation
-                _explanation.emit(explanation)
+                _response.emit(repository.pictureByDate(Util.getDateString(delta)))
             } catch (e: IOException) {
                 _error.emit(network_error)
             }
-
             _loading.emit(false)
         }
     }
